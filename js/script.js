dd9381ca88f0ad271215ffa600e710b0d635e499
Anton <flamer721@gmail.com>
2023-04-07 18:13:56 +0300

    feat: add const personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 const numberOfFilms = promt("Сколько фильмов уже посмотрели?", 0);
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
