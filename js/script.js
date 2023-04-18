df1ab7f8890d33324646c11b61b8d88fbefc7d68
Anton <flamer721@gmail.com>
2023-04-18 18:08:16 +0300

    task 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const numberOfFilms = +prompt("Сколько фильмов уже посмотрели?", 0);
+"use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов уже посмотрели?", "");
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,12 +10,25 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = +prompt("На сколько оцените его?", "");
-const c = prompt("Один из последних просмотренных фильмов?", "");
-const d = +prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = +prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  if (a !== null && b !== null && a !== "" && b !== "" && a.length <= 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотренно довольно мало фильмов");
+} else if (10 <= personalMovieDB.count <= 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB.movies);
